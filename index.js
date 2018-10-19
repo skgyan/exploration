3014d9450938a220d7a73477b79ae844dea21ecd
Sushil Kumar <skgyan@gmail.com>
2018-10-19 12:00:46 +0530

    override console.log method

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import method from "./method_joiner";
 import once from "./run_only_once";
+import log from "./override_console_log";
 
-method();
-once();
+// method();
+// once();
+log("hi");
+// just trying random things here
